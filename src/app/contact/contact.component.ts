6b30c06c7213e543d93a9dfd7fc7ca344f3f2ab7
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-12-22 14:28:06 +0530

    ng-basics

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { ComponentFixture } from '@angular/core/testing';
 
 @Component({
     selector: 'app-contact',
-    template: '<p>Contact component works!</p>'
+    template: '<p>contact works!</p>'
 })
 
 export class ContactComponent {
