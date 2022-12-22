ae48e9e407cdfa7a4706a3b9326335fd944de237
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-12-22 14:08:55 +0530

    ng-basics

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  // templateUrl: './home.component.html',
+  template: '<p>Home works...!</p>',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
