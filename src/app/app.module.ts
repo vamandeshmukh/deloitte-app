7c08853be01072ee8fb1f8f9f0719c266e4442dc
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-12-22 14:16:46 +0530

    ng-basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ComponentFixture } from '@angular/core/testing';
+
+@Component({
+    selector: 'app-contact',
+    template: '<p>Contact component works!</p>'
+})
+
+export class ContactComponent {
+
+}
