2b91149d3c99c647173c6c6bb29431d6046f6a41
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-12-23 10:36:37 +0530

    ng-basics

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,9 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
-  
 
-  myList = [  ];
+
+  empList = [
+    { eid: 101, fName: 'Sonu', salary: 90000 },
+    { eid: 102, fName: 'Monu', salary: 95000 },
+    { eid: 103, fName: 'Tonu', salary: 99000 },
+    { eid: 104, fName: 'Gonu', salary: 98000 },
+  ];
 
   myNums = [25, 31, 9, 22];
 
