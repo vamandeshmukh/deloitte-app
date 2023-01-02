8fc3932f4fbd2a50fdf7a6cf0b303443ef196196
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-01-02 20:03:15 +0530

    @Input

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -10,4 +10,29 @@ export class ChildComponent {
   @Input()
   parentDataInChild: string = '';
 
+
 }
+
+// import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+// @Component({
+//   selector: 'app-child',
+//   templateUrl: './child.component.html',
+//   styleUrls: ['./child.component.css']
+// })
+// export class ChildComponent {
+
+//   @Input()
+//   parentDataInChild: string = '';
+
+//   childData: string = 'def';
+
+//   @Output()
+//   sendDataToParent: EventEmitter<any> = new EventEmitter<any>();
+
+//   clickToSend = () => {
+//     console.log(this.childData);
+//     this.sendDataToParent.emit(this.childData);
+//   };
+
+// }
