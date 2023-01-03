d34932d6f1a3a4de994b301abc3e428a4ef48863
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-01-03 12:34:52 +0530

    service

---
+++ src/app/backend.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackendService {
+
+  // http requests 
+  // GET, POST, ... 
+
+  
+
+  constructor() { }
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,67 +1,78 @@
-import { Component, OnInit } from '@angular/core';
-// import { FormGroup, FormControl } from '@angular/forms';
-
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
-})
-export class LoginComponent  {
-
-  loggedInData = {
-    userName: '',
-    password: ''
-  };
-
-  submittedData = {
-    userName: '',
-    password: ''
-  };
-
-  submitLogin = (login: any) => {
-    console.log(login.value);
-    this.submittedData = login.value;
-  };
-
-}
-
-
-   
-
-// import { Component, OnInit } from '@angular/core';
-// import { FormGroup, FormControl } from '@angular/forms';
+// import { Component} from '@angular/core';
 
 // @Component({
 //   selector: 'app-login',
 //   templateUrl: './login.component.html',
 //   styleUrls: ['./login.component.css']
 // })
-// export class LoginComponent implements OnInit {
+// export class LoginComponent  {
 
-//   loginForm: FormGroup;
-
-//   constructor() {
-//     this.loginForm = new FormGroup({});
-//   };
- 
 //   loggedInData = {
 //     userName: '',
 //     password: ''
 //   };
 
-//   ngOnInit(): void {
-//     this.loginForm = new FormGroup({
-//       userName: new FormControl(''),
-//       password: new FormControl('')
-//     });
-//   }
+//   submittedData = {
+//     userName: '',
+//     password: ''
+//   };
 
-//   submitLogin = () => {
-//     this.loggedInData = this.loginForm.value;
-//     console.log(this.loginForm.value);
+//   submitLogin = (login: any) => {
+//     console.log(login.value);
+//     this.submittedData = login.value;
 //   };
 
 // }
 
 
-   
+
+
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+
+  // 1 
+  constructor() {
+    this.loginForm = new FormGroup({});
+  };
+
+  loggedInData = {
+    userName: '',
+    password: ''
+  };
+
+  // 2 
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      userName: new FormControl(''),
+      password: new FormControl('')
+    });
+  }
+
+  submitLogin = () => {
+    this.loggedInData = this.loginForm.value;
+    console.log(this.loginForm.value);
+  };
+
+}
+
+// https://stackoverflow.com/questions/35763730/difference-between-constructor-and-ngoninit
+
+
+// method === function === hook === subroutine === routine 
+
+// angular lifecycle hooks 
+
+
+
+
+
+
