d82d6d9e10aae981b49eb630940f371cbf7f156e
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-01-03 11:56:13 +0530

    template driven form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,32 +6,62 @@ import { FormGroup, FormControl } from '@angular/forms';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
 
-  loginForm: FormGroup;
-
-  constructor() {
-    this.loginForm = new FormGroup({});
-  };
- 
   loggedInData = {
     userName: '',
     password: ''
   };
 
-  ngOnInit(): void {
-    this.loginForm = new FormGroup({
-      userName: new FormControl(''),
-      password: new FormControl('')
-    });
-  }
+  submittedData = {
+    userName: '',
+    password: ''
+  };
 
-  submitLogin = () => {
-    this.loggedInData = this.loginForm.value;
-    console.log(this.loginForm.value);
+  submitLogin = (login: any) => {
+  this.submittedData = login.value;
+    console.log(login.value);
   };
 
 }
 
 
-  
+   
+
+// import { Component, OnInit } from '@angular/core';
+// import { FormGroup, FormControl } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.css']
+// })
+// export class LoginComponent implements OnInit {
+
+//   loginForm: FormGroup;
+
+//   constructor() {
+//     this.loginForm = new FormGroup({});
+//   };
+ 
+//   loggedInData = {
+//     userName: '',
+//     password: ''
+//   };
+
+//   ngOnInit(): void {
+//     this.loginForm = new FormGroup({
+//       userName: new FormControl(''),
+//       password: new FormControl('')
+//     });
+//   }
+
+//   submitLogin = () => {
+//     this.loggedInData = this.loginForm.value;
+//     console.log(this.loginForm.value);
+//   };
+
+// }
+
+
+   
